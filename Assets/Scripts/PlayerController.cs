85a3e7fdc93308da4c5f4067fbe53b65814ad859
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-14 14:40:09 +0100

    Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive!");
+    }
+
+    public Rigidbody rb;
+
+    // Speed of movement
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+
+        // Arrow keys
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+
+
+        // awsd keys
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+    }
+}
