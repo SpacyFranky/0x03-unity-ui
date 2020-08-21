0a573e4a0615bbeea475c8df6d52c2e492ed1ed7
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-21 18:04:23 +0100

    win, lose or draw box

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,8 +24,6 @@ public class PlayerController : MonoBehaviour
     // Public variable to store the score of the healthText UI gameobject.
     public Text healthText;
 
-
-
     // Update is called when health equals to 0.
     void Update()
     {
