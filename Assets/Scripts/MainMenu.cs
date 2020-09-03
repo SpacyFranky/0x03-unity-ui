a7aad84ea40bf98cfa1bd83d5dd44b6595a9b39b
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-09-03 11:53:33 +0100

    Updated script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
+    // Public variable to store the PlayButton UI gameobject
+    public Button play;
+    // Public variable to store the QuitButton UI gameobject
+    public Button quit;
+
+    void Start()
+    {
+        PlayMaze();
+        QuitMaze();
+    }
+    
     // loads the maze scene when the Play button is pressed
     public void PlayMaze()
     {
-        SceneManager.LoadScene("maze");
+        play.onClick.AddListener(() => SceneManager.LoadScene("maze"));
     }
 
     // Closes the game window when the Quit button is pressed
     public void QuitMaze()
+    {
+        quit.onClick.AddListener(QuitOnClick);
+    }
+
+    public void QuitOnClick()
     {
         Application.Quit();
         Debug.Log("Quit Game");
